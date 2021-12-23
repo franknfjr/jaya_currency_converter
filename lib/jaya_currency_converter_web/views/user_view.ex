b05ef101159cea2318410eb136e33f2a86ac61e9
franknfjr <frankdonascimentojr@gmail.com>
2021-12-23 03:39:43 -0300

    feat: user view

---
+++ lib/jaya_currency_converter_web/views/user_view.ex
@@ -0,0 +1,21 @@
+defmodule JayaCurrencyConverterWeb.UserView do
+  use JayaCurrencyConverterWeb, :view
+
+  alias JayaCurrencyConverterWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    render_many(users, UserView, "user.json")
+  end
+
+  def render("show.json", %{user: user}) do
+    render_one(user, UserView, "user.json")
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      name: user.name,
+      created_at: user.inserted_at
+    }
+  end
+end
