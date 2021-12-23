c1d5ae46834217d25a30e96faa5c5e0ed3bf4d51
franknfjr <frankdonascimentojr@gmail.com>
2021-12-23 03:40:17 -0300

    test: some tests

---
+++ test/jaya_currency_converter_web/views/jaya_currency_converter/accounts_test.exs
@@ -0,0 +1,40 @@
+defmodule JayaCurrencyConverter.AccountsTest do
+  use JayaCurrencyConverter.DataCase
+
+  alias JayaCurrencyConverter.Accounts
+
+  describe "users" do
+    alias JayaCurrencyConverter.Accounts.User
+
+    @valid_attrs %{name: "Frank Ferreira"}
+    @invalid_attrs %{name: nil}
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Accounts.create_user()
+
+      user
+    end
+
+    test "list_users/0 returns all users" do
+      user = user_fixture()
+      assert Accounts.list_users() == [user]
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      user = user_fixture()
+      assert Accounts.get_user!(user.id) == {:ok, user}
+    end
+
+    test "create_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+      assert user.name == "Frank Ferreira"
+    end
+
+    test "create_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+  end
+end
