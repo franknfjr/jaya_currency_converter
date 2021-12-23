f3a436c2fa6f43dfd3326da48a4a1d86082af61f
franknfjr <frankdonascimentojr@gmail.com>
2021-12-23 03:39:33 -0300

    feat: view transaction with nested data

---
+++ lib/jaya_currency_converter_web/views/transaction_view.ex
@@ -0,0 +1,26 @@
+defmodule JayaCurrencyConverterWeb.TransactionView do
+  use JayaCurrencyConverterWeb, :view
+
+  alias JayaCurrencyConverterWeb.TransactionView
+  
+  def render("index.json", %{transactions: transactions}) do
+    render_many(transactions, TransactionView, "transaction.json")
+  end
+  
+  def render("show.json", %{transaction: transaction}) do
+    render_one(transaction, TransactionView, "transaction.json")
+  end
+  
+  def render("transaction.json", %{transaction: transaction}) do
+    %{
+      id: transaction.id,
+      currency_from: transaction.currency_from,
+      currency_to: transaction.currency_to,
+      amount: transaction.amount,
+      rate: transaction.rate,
+      user_id: transaction.user_id,
+      amount_to: transaction.amount_to,
+      created_at: transaction.inserted_at
+    }
+  end
+end
