b93a85f25d34b74f759c6e610261a2d3a5eafd11
franknfjr <frankdonascimentojr@gmail.com>
2021-12-24 23:19:23 -0300

    style: mix format

--- test/jaya_currency_converter_web/controller/user_controller_test.exs
+++ test/jaya_currency_converter_web/controller/user_controller_test.exs
@@ -1,27 +1,27 @@
 defmodule JayaCurrencyConverterWeb.UserControllerTest do
   use JayaCurrencyConverterWeb.ConnCase
 
-#   describe "transactions" do
-#     @valid_user %{
-#       name: "Frank"
-#     }
+  #   describe "transactions" do
+  #     @valid_user %{
+  #       name: "Frank"
+  #     }
 
-#     test "POST create an user /", %{conn: conn} do
-#       conn =
-#         conn
-#         |> post(Routes.user_path(conn, :create), user: @valid_user)
+  #     test "POST create an user /", %{conn: conn} do
+  #       conn =
+  #         conn
+  #         |> post(Routes.user_path(conn, :create), user: @valid_user)
 
-#       assert %{id: id} = redirected_params(conn)
-#       assert redirected_to(conn) == Routes.user_path(conn, :show, id)
+  #       assert %{id: id} = redirected_params(conn)
+  #       assert redirected_to(conn) == Routes.user_path(conn, :show, id)
 
-#     #   conn = get(conn, Routes.user_path(conn, :show, id))
-#     end
+  #     #   conn = get(conn, Routes.user_path(conn, :show, id))
+  #     end
 
-    # test "GET all users /", %{conn: conn} do
-    # end
+  # test "GET all users /", %{conn: conn} do
+  # end
 
-    # test "GET show a user /", %{conn: conn} do
-    #   user = JayaCurrencyConverter.Accounts.get_user()
-    # end
-#   end
+  # test "GET show a user /", %{conn: conn} do
+  #   user = JayaCurrencyConverter.Accounts.get_user()
+  # end
+  #   end
 end
